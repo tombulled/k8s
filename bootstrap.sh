a3d163ee5e7fd371e002a488f57039b9684de8a9
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2026-05-31 10:14:33 -0400

    feat: configure gateway-api-crds

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,13 @@
 # 1. Build CLuster
 K3D_FIX_DNS=1 k3d cluster create --config cluster.yaml
 
-# 2. Install Sealed Secrets
+# 2. Install Gateway API CRDs
+helm install \
+    gateway-api-crds \
+    charts/gateway-api-crds \
+    -f ../values/gateway-api-crds/values.yaml
+
+# 3. Install Sealed Secrets
 kubectl create namespace sealed-secrets
 kubectl apply -f secrets/sealed-secrets/secret.yaml -n sealed-secrets
 helm install \
@@ -14,7 +20,7 @@ helm install \
     -n sealed-secrets \
     -f ../values/sealed-secrets/values.yaml
 
-# 3. Install ArgoCD
+# 4. Install ArgoCD
 helm install \
     argo-cd \
     argo-cd \
@@ -24,9 +30,9 @@ helm install \
     -f ../values/argo-cd/values.yaml \
     --create-namespace
 
-# 4. Install ArgoCD Repo Credential (Not managed via Sealed Secrets for now as the keys are public)
+# 5. Install ArgoCD Repo Credential (Not managed via Sealed Secrets for now as the keys are public)
 kubectl -n argocd create secret generic github-ssh --from-literal=url=git@github.com:tombulled --from-file=sshPrivateKey=$HOME/.ssh/id_rsa
 kubectl -n argocd label secret github-ssh argocd.argoproj.io/secret-type=repo-creds
 
-# 5. Install Root App
+# 6. Install Root App
 kubectl apply -f root.yaml
